2997be3dc77612a58df0d8e052039710e3766915
paulway <paulway@6bb8afd2-ee04-0410-8f1a-d3aebcb25db2>
2012-12-25 10:49:35 +0000

    Peter Miller: including standard short-form GPL v2 license header.
    git-svn-id: http://tangram.dnsalias.net/repos/pwlib-c/cfile@86 6bb8afd2-ee04-0410-8f1a-d3aebcb25db2

--- _info.c
+++ _info.c
@@ -1,15 +1,25 @@
+/*
+ * cfile - compressed file read/write library
+ * Copyright (C) 2006 Paul Wayper
+ * Copyright (C) 2012 Peter Miller
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <string.h>
-#include "config.h"
 
-/**
- * cfile - compressed file read/write library.
- *
- * C file library that provides transparent read/write access to
- * gzip and bzip2 compressed files.
- *
- * Licence: GPL (2 or any later version)
- */
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
@@ -22,3 +32,6 @@ int main(int argc, char *argv[])
 
 	return 1;
 }
+
+
+/* vim: set ts=8 sw=4 et : */
