97d6de573293b6de605046d700956880d9d497ea
paulway <paulway@6bb8afd2-ee04-0410-8f1a-d3aebcb25db2>
2013-06-23 16:10:41 +0000

    Adding license text and function prototypes.
    git-svn-id: http://tangram.dnsalias.net/repos/pwlib-c/cfile@91 6bb8afd2-ee04-0410-8f1a-d3aebcb25db2

--- cfile_bzip2.h
+++ cfile_bzip2.h
@@ -1,2 +1,32 @@
-cfile *bzip2_open(const char *name, const char *mode);
+//
+// cfile - compressed file read/write library
+// Copyright (C) 2006 Paul Wayper
+// Copyright (C) 2012 Peter Miller
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or (at
+// your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
 
+#ifndef CFILE_BZIP2_H
+#define CFILE_BZIP2_H
+
+cfile *bzip2_open(const char *name, const char *mode);
+off_t bzip2_size(cfile *fp);
+int bzip2_eof(cfile *fp);
+char *bzip2_gets(cfile *fp, char *str, int len);
+ssize_t bzip2_read(cfile *fp, void *ptr, size_t size, size_t num);
+ssize_t bzip2_write(cfile *fp, const void *ptr, size_t size, size_t num);
+int bzip2_flush(cfile *fp);
+int bzip2_close(cfile *fp);
+
+#endif
--- cfile_gzip.h
+++ cfile_gzip.h
@@ -1,2 +1,32 @@
-cfile *gzip_open(const char *name, const char *mode);
+//
+// cfile - compressed file read/write library
+// Copyright (C) 2006 Paul Wayper
+// Copyright (C) 2012 Peter Miller
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or (at
+// your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
 
+#ifndef CFILE_GZIP_H
+#define CFILE_GZIP_H
+
+cfile *gzip_open(const char *name, const char *mode);
+off_t gzip_size(cfile *fp);
+int gzip_eof(cfile *fp);
+char *gzip_gets(cfile *fp, char *str, int len);
+ssize_t gzip_read(cfile *fp, void *ptr, size_t size, size_t num);
+ssize_t gzip_write(cfile *fp, const void *ptr, size_t size, size_t num);
+int gzip_flush(cfile *fp);
+int gzip_close(cfile *fp);
+
+#endif
