9eafd9ae52ca46fba28ac614dac2c29b5a0585a8
Paul Wayper <paulway@mabula.net>
2014-01-12 09:15:21 +0800

    Renaming to provide standardised LZO open function.

--- cfile_lzo.h
+++ cfile_lzo.h
@@ -16,35 +16,22 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef CFILE_NULL_H
-#define CFILE_NULL_H
+#ifndef CFILE_LZO_H
+#define CFILE_LZO_H
 
 #include "cfile_private.h"
 
 /**
-  * The cfile_null function is used to open a file that is always empty,
-  * and discards all output written to it.
+  * The cfile_lzo function is used to open LZO files.
   *
   * @param pathname
-  *     The name of the file of interest (ignored).
+  *     The name of the file to open.
   * @param mode
-  * 	The mode to use for file operations (ignored).
+  * 	The mode to use for file operations (read or write).
   * @returns
   *     The new file handle
   */
-cfile *cfile_null_open(const char *pathname, const char *mode);
-
-/**
-  * The cfile_null_candidate function is used to determine whether or
-  * not the given file looks like the /dev/null device.
-  *
-  * @param pathname
-  *     The pat of the file to examine.
-  * @returns
-  *     true (non-zero) if the file is a candidate, or
-  *     false (zero) if it is not.
-  */
-int cfile_null_candidate(const char *pathname);
+cfile *cfile_lzo_open(const char *pathname, const char *mode);
 
 /* vim: set ts=8 sw=4 et : */
-#endif /* CFILE_NULL_H */
+#endif /* CFILE_LZO_H */
