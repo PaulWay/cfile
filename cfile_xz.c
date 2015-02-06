2cbd32fd3e58a9003459fa2d76ecb2174e3f7d23
Paul Wayper <paulway@redhat.com>
2015-02-06 10:45:05 +1000

    lzma_index_cat now takes a final argument which is the padding. Not sure what to put here, since it has to be a multiple of four - I chose '4'.

--- cfile_xz.c
+++ cfile_xz.c
@@ -299,7 +299,8 @@ static off_t xz_size(cfile *fp) {
         
         /* combine indexes if we have two */
         if (combined_index != NULL) {
-            rtn = lzma_index_cat(this_index, combined_index, NULL);
+			/* this, other, allocator, padding (%4==0) */
+            rtn = lzma_index_cat(this_index, combined_index, NULL, 4);
             if (rtn != LZMA_OK) {
                 break;
             }
